de721985a885580d195b73dc966ccc18d0b33158
YouWei Zhao <zhao.uv@gmail.com>
2018-01-29 16:48:13 +0800

    Update Visitors.js

--- README.md
+++ README.md
@@ -3,18 +3,25 @@
 
 同时用来尝试用[antlr](https://github.com/antlr/antlr4)来完成自动的.g4转[blockly](https://github.com/google/blockly)的过程
 
+目前进度
++ 能够用BlocklyGrammer.g4解析MotaAction.g4
++ SymbolVisitor能够解析所有符号
+
+- - -
+
 blockly中mutators太过麻烦不适宜自动化,放弃
 
 > blockly中`numeric imput`的机制待研究.
 
 可以借助ChangeListener实现
-
+```
+b.getFieldValue()
 b.setFieldValue('NUM',123)
 b.getInputTargetBlock('statements')
 
-b=workspace.getBlockById(event.blockId)
+b=demoWorkspace.getBlockById(event.blockId)
 event.type == Blockly.Events.CHANGE && b.type == 'xxxx'
-
+```
 blockly.xml需要仔细研究一下
 
 函数声明和变量声明也要再看一看
--- Visitors.js
+++ Visitors.js
@@ -12,10 +12,37 @@ SymbolVisitor.prototype.constructor = SymbolVisitor;
 
 SymbolVisitor.prototype.init = function() {
   this.statementRules=[];
-  this.expressionRules=[[],[]];//0 for type: expression, 1 for block: value
-  this.lexerRules=[];
+  this.expressionRules=[];
   this.expression_arithmetic_num=0;
+  this.lexerRules={};
   return this;
+}
+
+SymbolVisitor.prototype.error = function(error) {
+  throw(new Error(error));
+}
+
+SymbolVisitor.prototype.checkSymbol = function() {
+  var filter_ = function(list,name) {
+    for(var ii=0,value;value=list[ii];ii++){
+      if (value[0]===name) return value[1];
+    }
+    return [];
+  }
+  for(var ii=0,statementRule;statementRule=this.statementRules[ii];ii++){
+    if (statementRule[1].length>1){
+      for(var jj=0,statname;statname=statementRule[1][jj];jj++){
+        if (filter_(this.statementRules,statname).length>1){
+          this.error(statementRule[0]+' 下的子规则 '+statname+' 包含了"|"');
+        }
+      }
+    }
+  }
+  for(var ii=0,expressionRule;expressionRule=this.expressionRules[ii];ii++){
+    if (expressionRule[1].length>1 && expressionRule[0]!=='expression'){
+      this.error('表达式 '+expressionRule[0]+' 包含了"|"');
+    }
+  }
 }
 
 // Visit a parse tree produced by BlocklyGrammerParser#grammarFile.
@@ -23,7 +50,7 @@ SymbolVisitor.prototype.visitGrammarFile = function(ctx) {
   this.visit(ctx.statementRule());
   this.visit(ctx.expressionRule());
   this.visit(ctx.lexerRuleCollection(0));
-  return Object.assign({},this);
+  return this;
 };
 
 // Visit a parse tree produced by BlocklyGrammerParser#StatList.
@@ -49,20 +76,20 @@ SymbolVisitor.prototype.visitExprList = function(ctx) {
   for(var ii=0;ii<this.expression_arithmetic_num;ii++){
     var exprname = 'expression_arithmetic_'+ii;
     exprs.push(exprname);
-    this.expressionRules[1].push(exprname);
+    this.expressionRules.push([exprname,[exprname]]);
   }
   var pIds = ctx.ParserIdentifier();
   for(var ii=0,value;value=pIds[ii];ii++){
     pIds[ii]=value.getText();
   }
   exprs=exprs.concat(pIds);
-  this.expressionRules[0]=['expression',exprs];
+  this.expressionRules.push(['expression',exprs]);
 };
 
 // Visit a parse tree produced by BlocklyGrammerParser#ExprValue.
 SymbolVisitor.prototype.visitExprValue = function(ctx) {
   var exprId = ctx.ParserIdentifier(0).getText();
-  this.expressionRules[1].push(exprId);
+  this.expressionRules.push([exprId,[exprId]]);
 };
 
 // Visit a parse tree produced by BlocklyGrammerParser#lexerRuleCollection.
@@ -70,7 +97,7 @@ SymbolVisitor.prototype.visitLexerRuleCollection = function(ctx) {
   var lexerRules = ctx.lexerRule() || [];
   for(var ii=0,value;value=lexerRules[ii];ii++){
     var lexerId = value.LexerIdentifier(0).getText();
-    this.lexerRules.push(lexerId);
+    this.lexerRules[lexerId]=null;
   }
 };
 
@@ -83,22 +110,58 @@ function EvalVisitor() {
 EvalVisitor.prototype = Object.create(BlocklyGrammerVisitor.prototype);
 EvalVisitor.prototype.constructor = EvalVisitor;
 
+/**
+ * @param {!SymbolVisitor} symbols SymbolVisitor.
+ */
 EvalVisitor.prototype.init = function(symbols) {
   this.statementRules=symbols.statementRules;
   this.expressionRules=symbols.expressionRules;
-  //0 for type: expression, 1 for block: value
   this.lexerRules=symbols.lexerRules;
   this.expression_arithmetic_num=symbols.expression_arithmetic_num;
   return this;
 }
 
+EvalVisitor.prototype.setRule = function(type,name,value) {
+  if (type==='lexer') {
+    this.lexerRules[name]=value;
+  }
+}
+
+EvalVisitor.prototype.escapeString = function(string_) {
+  return eval(string_);
+}
+
 // Visit a parse tree produced by BlocklyGrammerParser#grammarFile.
 EvalVisitor.prototype.visitGrammarFile = function(ctx) {
+  this.visit(ctx.lexerRuleCollection(0));
   this.visit(ctx.statementRule());
   this.visit(ctx.expressionRule());
-  this.visit(ctx.lexerRuleCollection(0));
-  return Object.assign({},this);
 };
+
+// Visit a parse tree produced by BlocklyGrammerParser#strings.
+EvalVisitor.prototype.visitStrings = function(ctx) {
+  var Strings = ctx.String();
+  for(var ii=0,value;value=Strings[ii];ii++){
+    Strings[ii]=this.escapeString(value.getText());
+  }
+  return Strings.join(' ');
+};
+
+// Visit a parse tree produced by BlocklyGrammerParser#LexerRuleStrings.
+EvalVisitor.prototype.visitLexerRuleStrings = function(ctx) {
+  var lexerId = ctx.LexerIdentifier(0).getText();
+  var strings = this.visit(ctx.strings(0));
+  this.setRule('lexer',lexerId,strings);
+};
+
+// Visit a parse tree produced by BlocklyGrammerParser#LexerRuleList.
+EvalVisitor.prototype.visitLexerRuleList = function(ctx) {
+  return this.visitChildren(ctx);
+};
+
+// Visit a parse tree produced by BlocklyGrammerParser#LexerRuleComplex.
+EvalVisitor.prototype.visitLexerRuleComplex = function(ctx) {};
+
 
 exports.SymbolVisitor = SymbolVisitor;
 exports.EvalVisitor = EvalVisitor;
