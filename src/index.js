23c6f4615f705d25f3aaea3c503a36b0e0fd05f2
YouWei Zhao <zhao.uv@gmail.com>
2020-10-07 10:37:35 +0800

    fix entrance for webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-exports.Converter = require('./Converter').Converter;
-if (typeof window!=='undefined') {
-    window.Converter = require('./Converter').Converter;
-}
+// Entrance for webpack, do not use for other cases
+
+// Strange but working format
+// To make the bundle.min.js available in both browser and node
+export let Converter = require('./Converter').Converter;
+if (typeof exports!=='undefined') exports.Converter = require('./Converter').Converter;
+if (typeof window!=='undefined') window.Converter = require('./Converter').Converter;
